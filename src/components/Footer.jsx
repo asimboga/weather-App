7e88b5e4bcd51eefef032bfb4e969e22f403509a
asimboga <asimboga@gmail.com>
2022-07-09 00:35:45 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+    <div className="container">
+      <small>Coded by Asim Boga
+      </small>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
